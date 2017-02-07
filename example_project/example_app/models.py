45e76572dcf512514dacf827dfeec2ec0ee7f812
Zachary Kent <zak.kent@gmail.com>
2017-02-07 14:02:01 -0800

    added song data to sqlite db

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Songs(models.Model):
     id = models.AutoField(primary_key=True)
     artist_name = models.CharField(max_length=255, default='')
+    song_title = models.CharField(max_length=255, default='')
     song_year = models.IntegerField(blank=True, null=True)
     song_lyrics = models.TextField()
 
